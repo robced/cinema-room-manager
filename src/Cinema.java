9e62ea00dbb9400ce57ce710bb0a9da37197b0a5
Devyos <e@robced.com>
2023-11-27 23:18:40 -0800

    feat: cinema room manager

---
+++ src/Cinema.java
@@ -0,0 +1,92 @@
+import java.util.Arrays;
+
+public class Cinema {
+    private final int numberOfRows;
+    private final int numberOfColumns;
+
+    private int purchasedTickets = 0;
+    private int currentIncome = 0;
+    private int numberOfSeats = 0;
+    private int totalIncome = 0;
+    private String[][] cinemaSeats;
+
+
+    public Cinema(int numberOfRows, int numberOfColumns) {
+        this.cinemaSeats = buildCinemaRoom(numberOfRows, numberOfColumns);
+        this.numberOfRows = cinemaSeats.length;
+        this.numberOfColumns = cinemaSeats[0].length;
+        this.numberOfSeats = calculateNumberOfSeats(cinemaSeats);
+        this.totalIncome = calculateTotalIncome(cinemaSeats,numberOfSeats);
+    }
+
+    private static String[][] buildCinemaRoom(int numberOfRows, int numberOfColumns) {
+        String[][] cinemaRoom = new String[numberOfRows][numberOfColumns];
+
+        for (String[] strings : cinemaRoom) {
+            Arrays.fill(strings, "S");
+        }
+
+        return cinemaRoom;
+    }
+
+    public void buyTicket(int selectedRow, int selectedColumn) {
+        if (selectedRow >= cinemaSeats.length || selectedColumn >= cinemaSeats[0].length) {
+            throw new IllegalArgumentException("Wrong input!");
+        }
+
+        if ("B".equals(cinemaSeats[selectedRow][selectedColumn])) {
+            throw new IllegalStateException("That ticket has already been purchased!");
+        }
+
+        cinemaSeats[selectedRow][selectedColumn] = "B";
+        purchasedTickets++;
+        currentIncome += calculateTicketPrice(selectedRow);
+    }
+
+    public int calculateTicketPrice(int selectedRow) {
+        int frontRow = calculateFrontRow(cinemaSeats);
+        return numberOfSeats < 60 ? 10 : (selectedRow < frontRow ? 10 : 8);
+    }
+
+    private static int calculateNumberOfSeats(String[][] cinemaSeats) {
+        return cinemaSeats.length * cinemaSeats[0].length;
+    }
+
+    private static int calculateFrontRow(String[][] cinemaSeats) {
+        return (int) Math.floor(cinemaSeats.length / 2.0d);
+    }
+
+    private static int calculateTotalIncome(String[][] cinemaSeats, int numberOfSeats) {
+        if (numberOfSeats < 60) {
+            return numberOfSeats * 10;
+        }
+
+        int frontRow = calculateFrontRow(cinemaSeats);
+        int backRow = cinemaSeats.length - frontRow;
+
+        int frontRowSeatsIncome = frontRow * cinemaSeats[0].length * 10;
+        int backRowSeatsIncome = backRow * cinemaSeats[0].length * 8;
+
+        return frontRowSeatsIncome + backRowSeatsIncome;
+    }
+
+    public String[][] getRoomMap() {
+        return cinemaSeats;
+    }
+
+    public int getPurchasedTickets() {
+        return purchasedTickets;
+    }
+
+    public float getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public int getCurrentIncome() {
+        return currentIncome;
+    }
+
+    public int getTotalIncome() {
+        return totalIncome;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,110 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Cinema cinemaRoom = buildCinemaRoom(scanner);
+        String[][] cinemaRoomMap = cinemaRoom.getRoomMap();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        printMenu(scanner, cinemaRoomMap, cinemaRoom);
+    }
+
+    private static Cinema buildCinemaRoom(Scanner scanner) {
+        System.out.println("Enter the number of rows:");
+        int numberOfRows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int numberOfColumns = scanner.nextInt();
+        System.out.println();
+
+        return new Cinema(numberOfRows, numberOfColumns);
+    }
+
+    public static void printCinemaSeats(String[][] cinemaRoomMap) {
+        System.out.println("Cinema: ");
+        System.out.print("  ");
+
+        for (int i = 1; i <= cinemaRoomMap[0].length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < cinemaRoomMap.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j= 0; j < cinemaRoomMap[i].length; j++) {
+                System.out.print(cinemaRoomMap[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+    private static void  buyTicket(Scanner scanner, Cinema cinemaRoom) {
+        System.out.println("Enter a row number:");
+        int selectedRow = scanner.nextInt() - 1;
+        System.out.println("Enter a seat number in that row:");
+        int selectedColumn = scanner.nextInt() - 1;
+
+        try {
+            cinemaRoom.buyTicket(selectedRow, selectedColumn);
+            System.out.println();
+
+            System.out.println("Ticket price: $" + cinemaRoom.calculateTicketPrice(selectedRow));
+            System.out.println();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println();
+            buyTicket(scanner, cinemaRoom);
+        }
+    }
+
+    private static void printStatistics(String[][] cinemaRoomMap, Cinema cinemaRoom) {
+        int purchasedTickets = cinemaRoom.getPurchasedTickets();
+        System.out.println("Number of purchased tickets: " + purchasedTickets);
+        System.out.printf("Percentage: %.2f%%%n", ( (float) purchasedTickets / cinemaRoom.getNumberOfSeats() * 100 ));
+        System.out.println("Current income: $" + cinemaRoom.getCurrentIncome());
+        System.out.println("Total income: $" + cinemaRoom.getTotalIncome());
+        System.out.println();
+    }
+
+    private static void printMenu(Scanner scanner, String[][] cinemaRoomMap, Cinema cinemaRoom) {
+        while (true) {
+            System.out.println("1. Show the seats");
+            System.out.println("2. Buy a ticket");
+            System.out.println("3. Statistics");
+            System.out.println("0. Exit");
+
+            int userMenuOption = scanner.nextInt();
+
+            if (userMenuOption < 0 || userMenuOption > 3) {
+                System.out.println("Wrong input.");
+                continue;
+            }
+
+            System.out.println();
+
+            switch (userMenuOption) {
+                case 1: {
+                    printCinemaSeats(cinemaRoomMap);
+                    break;
+                }
+
+                case 2: {
+                    buyTicket(scanner, cinemaRoom);
+                    break;
+                }
+
+                case 3: {
+                    printStatistics(cinemaRoomMap, cinemaRoom);
+                    break;
+                }
+
+                case 0: {
+                    return;
+                }
+            }
         }
     }
 }
